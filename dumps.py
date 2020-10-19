d3b9eb19a7fed691b468bc69d1cfc983d8e5abd6
TheErrorMaster <61210645+TheErrorMaster@users.noreply.github.com>
2020-10-18 20:13:10 -0700

    correct

--- dumps.py
+++ dumps.py
@@ -5,14 +5,14 @@ def _url(path, key):
     return "{}{}".format(path,key)
 
 def dump_jsons(p_url):
-    response = requests.get(p_url)
+    try:
+        response = requests.get(p_url)
         info = response.json()["keys"] # puts keys in a list
         # prints all keys values here
         for i in info:
             print(requests.get(_url(p_url,i)).json())
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-
 
 if __name__ == "__main__":
     dump_jsons(sys.argv[1])
